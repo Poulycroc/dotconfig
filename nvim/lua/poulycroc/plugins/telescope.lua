c912b6d07bfd889ba4c81cdfe63ce652651ee1a8
Poulycroc <esga.retard@gmail.com>
2024-12-22 12:56:55 +0100

    add toggle terminal + update telescope keymap

--- nvim/lua/poulycroc/plugins/telescope.lua
+++ nvim/lua/poulycroc/plugins/telescope.lua
@@ -67,7 +67,7 @@ return {
     map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live grep" })
     map("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor" })
     map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Telescope Find buffers" })
-    map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Telescope Help page" })
+    map("n", "<leader>fh", "<cmd>Telescope git_bcommits<CR>", { desc = "Telescope Git file history" })
     map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "Telescope Find marks" })
     map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Telescope Find oldfiles" })
     map(
@@ -80,6 +80,7 @@ return {
     map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope Git status" })
     map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "Telescope Pick hidden term" })
     map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "Telescope Nvchad themes" })
+    map("n", "<leader>fH", "<cmd>Telescope help_tags<CR>", { desc = "Telescope Help page" })
     map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope Find files" })
     map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
---
+++ nvim/lua/poulycroc/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = function()
+    local map = vim.keymap.set -- for conciseness
+
+    map("n", "<leader>tt", "<cmd>lua require('toggleterm').toggle()<CR>", {
+      desc = "Toggle terminal",
+      noremap = true,
+      silent = true,
+    })
+  end,
+}
